4e8003a51493e691bbca4cbe143bd23d1430463c
achuzz777 <76623656+achuzz777@users.noreply.github.com>
2021-07-27 13:26:13 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -63,7 +63,7 @@ async def run(bot, message):
             pass
    # await m.delete()
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk')
+        InlineKeyboardButton('Support 🚴‍♂️', url='https://t.me/Perfect_Vazha')
     ]] 
     reply_markup = InlineKeyboardMarkup(buttons)
     await m.edit(
